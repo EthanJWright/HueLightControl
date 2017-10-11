30d3670f6bde4a2ba332ba2497e475d6721b299b
Ethan Wright <ewright362@gmail.com>
2017-10-11 08:01:06 -0600

    app updates

--- APP/IoTController/app/src/main/java/com/iot/ethanwright/iotcontroller/MainActivity.java
+++ APP/IoTController/app/src/main/java/com/iot/ethanwright/iotcontroller/MainActivity.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Random;
+
 
 /**
  * Ethan Wright
@@ -20,6 +26,17 @@ import java.net.URL;
 
 public class MainActivity extends Activity {
     public String api_call;
+    public String[] adjectives =  {"dangerous","penitent","like","silly","military","difficult","equable","magical","lucky","exotic","fluffy","dashing","wonderful","future","axiomatic","outrageous","striped","dramatic","strong","spicy","faithful","splendid","tricky","delicate","jumpy","precious","decisive","teeny-tiny","talented","loud","astonishing","profuse","large","beneficial","various","regular","little","coherent","diligent","lively","unusual","fluttering","equal","spotted","luxuriant","adjoining","swift","bright","fast","gorgeous","tasteful","hushed","caring","square","bouncy","ambitious","dynamic","frail","fanatical","awesome","kind","cluttered","political","electric","cuddly","immense","reminiscent","fierce","enormous","thundering","selective","safe","learned","lazy","happy","long-term","towering","testy","silent","tranquil","grateful","elated","proud","aboriginal","powerful","high-pitched","waiting","violet","fabulous","plausible","supreme","different","youthful","momentous","decorous","fretful","distinct","purple","efficient","spiffy","nippy","aquatic"};
+    public String[] nouns = {"lumber","house","orange","shake","branch","yak","furniture","flock","zinc","yam","home boy","judge","flame","bird","baseball","burst","rainstorm","history","rod","grain","lamp","flower","cake","cobweb","idea","stream","pie","smile","pencil","plane","bird","snail","celery","mouse","truck","ink","camera","foot","tooth","loaf","card","sheet","flag","basketball","voice","minister","door","stew","maid","jelly","drawer","picture","hill","passenger","plastic","scale","nest","sister","invention","direction","horse","soup","wall","cow","committee","fork","man","toothpaste","dirt","bucket","moon","floor","paper","spy","engine","animal","cream","laborer","wine","cub","riddle","oatmeal","creature","insect","cracker","tongue","vacation","cloth","metal","volleyball"};
+
+    public void randomize(){
+        TextView randText = (TextView) findViewById(R.id.random);
+        Random rand = new Random();
+        String adjective = adjectives[rand.nextInt(adjectives.length)];
+        String noun = nouns[rand.nextInt(nouns.length)];
+        String message = "Hello my " + adjective + " " + noun + "!";
+        randText.setText(message);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +45,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Button lights_on = (Button)findViewById(R.id.hue_on);
         Button lights_off = (Button)findViewById(R.id.hue_off);
+
+
+        api_call = "{`check hue` : { `group` : `fan` } }".replace('`','"');
+        new AsyncUploadTest().execute();
+
+        randomize();
+
+
         lights_on.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,8 +68,34 @@ public class MainActivity extends Activity {
             }
         });
     }
+
     private class AsyncUploadTest extends AsyncTask<Void,Void,Boolean>
     {
+        private void setText(JSONObject mainObject) throws JSONException {
+            final ImageView light_icon = (ImageView)  findViewById(R.id.light_icon);
+            final Button lights_on = (Button) findViewById(R.id.hue_on);
+            final Button lights_off = (Button) findViewById(R.id.hue_off);
+            JSONObject hue = mainObject.getJSONObject("hue result");
+            JSONObject state = hue.getJSONObject("state");
+            final Boolean all_on = state.getBoolean("all_on");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(all_on){
+                        light_icon.setImageResource(R.drawable.ic_moon_on);
+                        lights_off.setElevation(12);
+                        lights_on.setElevation(6);
+
+                    }else{
+                        light_icon.setImageResource(R.drawable.ic_moon_off);
+                        lights_on.setElevation(12);
+                        lights_off.setElevation(6);
+                    }
+
+                }
+            });
+
+        }
         @Override
         protected Boolean doInBackground(Void... v) {
             try {
@@ -74,6 +125,9 @@ public class MainActivity extends Activity {
                 Log.d("Test","http response code: "+httpResult);
                 if(httpResult == HttpURLConnection.HTTP_OK)
                 {
+                    String text = IOUtils.toString(connection.getInputStream());
+                    JSONObject mainObject = new JSONObject(text);
+                    setText(mainObject);
                     Log.d("Test", IOUtils.toString(connection.getInputStream()));
                     Log.d("Test", "result read");
                 }
@@ -84,8 +138,10 @@ public class MainActivity extends Activity {
                 Log.e("Test","exception while sending data to server",e);
             } catch (IOException e) {
                 Log.e("Test","exception while sending data to server",e);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            return true;
+            return null;
         }
     }
 }
