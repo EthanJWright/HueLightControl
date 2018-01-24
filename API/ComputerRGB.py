6bbb852ba6c1a90ca6f9b61d7f87c2dd12132b38
EthanJWright <ewright362@gmail.com>
2018-01-24 04:24:21 +0000

    Fixed issue with property reference

--- API/ComputerRGB.py
+++ API/ComputerRGB.py
@@ -1,10 +1,15 @@
+import requests
+import ConfigParser
+
+
 class ComputerRGB():
     def __init__(self, _config):
         self.config = _config
 
-    def handle_request(json):
-        comp_pi = self.config.read('Computer Pi')
-        url = comp_pi + ':5000/'
+    def handle_request(self, json):
+        comp_pi = self.config.get('Computer Pi', 'ip')
+        print(comp_pi)
+        url = 'http://' + comp_pi + ':5000/'
         payload = json
         response = requests.post(url, json=payload).json()
         print response
