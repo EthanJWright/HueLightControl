211e3cdc64401858f9e7d1b1acbc833922542468
EthanJWright <ewright362@gmail.com>
2018-01-25 03:19:14 +0000

    fixed issue with api not turning off

--- API/CompAPI/StripController.py
+++ API/CompAPI/StripController.py
@@ -4,7 +4,7 @@ import LED_controller
 import time
 import json
 
-hostIP="192.168.1.15"
+hostIP="192.168.1.8"
 app = FlaskAPI(__name__)
 led = LED_controller.rgb([5,4,12])
 
@@ -17,6 +17,8 @@ def get_request():
             led.set(payload['rgb'])
             print payload['rgb']
             return_val = {'value' : 'success'}
+        if(payload.has_key('on') and not payload['on']):
+            led.set([0,0,0])
         return json.dumps(return_val)
     
 if __name__ == "__main__":
